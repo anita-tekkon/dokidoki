fb052a21c99976c619e390d5ef8e8b1a9bad331c
Anita <anita@tekkn.com.np>
2024-02-05 13:48:03 +0545

    change seo heads

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -10,13 +10,13 @@ const defaultMeta = {
   description:
     `Landing page ${COMPANY_NAME}`,
   // change base url of your web (without '/' at the end)
-  url: 'https://next-landing-vpn.vercel.app',
+  url: 'https://dokidoki-kohl.vercel.app',
   type: 'website',
   robots: 'follow, index',
   // change with url of your image (recommended dimension = 1.91:1)
   // used in twitter, facebook, etc. card when link copied in tweet/status 
-  image: 'https://next-landing-vpn.vercel.app/assets/card-image.png',
-  author: 'Lorem Ipsum'
+  image: 'https://dokidoki-kohl.vercel.app/assets/card-image.png',
+  author: 'DokiDoki'
 };
 
 /**
