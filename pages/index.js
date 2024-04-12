d2e8db8848a0af98c0b913b4c5dd6b577d4026d0
Anita <anita@tekkn.com.np>
2024-04-12 14:18:48 +0545

    .

--- pages/index.js
+++ pages/index.js
@@ -8,34 +8,34 @@ import Testimoni from "../components/Testimoni";
 
 export default function Home() {
 
-  return (
-    <>
-      {/* <SeoHead title={`${COMPANY_NAME} Landing Page`} /> */}
-      <div
-        style={{
-          height: '100vh',
-          margin: 'auto',
-          position: 'relative'
-        }}
-      >
+  // return (
+  //   <>
+  //     {/* <SeoHead title={`${COMPANY_NAME} Landing Page`} /> */}
+  //     <div
+  //       style={{
+  //         height: '100vh',
+  //         margin: 'auto',
+  //         position: 'relative'
+  //       }}
+  //     >
 
-        <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal"
+  //       <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal"
 
-          style={{
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%,-50%)'
-          }}>
-          <strong>OOPS </strong>
-          <br />
-          <p className="my-16"><strong>Wrong Site</strong>.</p>
+  //         style={{
+  //           position: 'absolute',
+  //           top: '50%',
+  //           left: '50%',
+  //           transform: 'translate(-50%,-50%)'
+  //         }}>
+  //         <strong>OOPS </strong>
+  //         <br />
+  //         <p className="my-16"><strong>Wrong Site</strong>.</p>
 
 
-        </h1>
-      </div >
-    </>
-  );
+  //       </h1>
+  //     </div >
+  //   </>
+  // );
   return (
     <>
       <SeoHead title={`${COMPANY_NAME} Landing Page`} />
